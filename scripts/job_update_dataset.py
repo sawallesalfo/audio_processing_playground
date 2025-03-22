f8987bb89ba2f7ce04ba1e0fe72d8b3d2167683d
Salif SAWADOGO <96554481+sawadogosalif@users.noreply.github.com>
2025-03-23 00:31:08 +0100

    👔  run after add HF_TOKEN in env vars

--- scripts/job_update_dataset.py
+++ scripts/job_update_dataset.py
@@ -25,7 +25,6 @@ def process_dataset(current_dataset_path, incoming_dataset_path, output_dataset_
         split="train", 
         download_config=DownloadConfig(token=hf_token)
     )
-
     # Chargement du dataset entrant depuis un stockage
     logger.info("Chargement du dataset entrant depuis le stockage...")
     incoming_dataset = load_from_disk(incoming_dataset_path, storage_options=storage_options)
