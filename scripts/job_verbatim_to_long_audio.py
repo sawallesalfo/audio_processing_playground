1f190316bad19d4b2fedf48159f722f1514205fb
Salif SAWADOGO <96554481+sawadogosalif@users.noreply.github.com>
2025-03-22 18:38:58 +0000

    :green_heart: CI vrom on aggreation

--- scripts/job_verbatim_to_long_audio.py
+++ scripts/job_verbatim_to_long_audio.py
@@ -76,17 +76,13 @@ def process_s3_audio_data(bucket_name: str, folder_to_process: str, output_path:
     combined_dataset = []
     files = list_s3_files(s3_client, bucket_name, folder_to_process)[1:]  # Skip the folder itself
     for file in tqdm(files):
-        try:
-            segmented_audio_folder = f"fasoai-segmented_audios/Sɩngre/{file.split('/')[-1].replace('.json','')}/"
-            file_path = f"{bucket_name}/{file}"
-            dataset = create_dataset_from_json(file_path, fs)
-            download_folder_from_s3(s3_client, bucket_name, segmented_audio_folder)
-            agg_dataset = dataset.map(mapper_function2, batched=True, batch_size=BATCH_SIZE, remove_columns=list(dataset.features))
-            agg_dataset = agg_dataset.add_column("audio_sequence", list(range(1, len(agg_dataset) + 1)))
-            combined_dataset.append(agg_dataset)
-            break
-        except Exception as e:
-            print(f"Error processing {file}: {e}")
+        segmented_audio_folder = f"fasoai-segmented_audios/Sɩngre/{file.split('/')[-1].replace('.json','')}/"
+        file_path = f"{bucket_name}/{file}"
+        dataset = create_dataset_from_json(file_path, fs)
+        download_folder_from_s3(s3_client, bucket_name, segmented_audio_folder)
+        agg_dataset = dataset.map(mapper_function2, batched=True, batch_size=BATCH_SIZE, remove_columns=list(dataset.features))
+        agg_dataset = agg_dataset.add_column("audio_sequence", list(range(1, len(agg_dataset) + 1)))
+        combined_dataset.append(agg_dataset)
     
     if combined_dataset:
         final_dataset = concatenate_datasets(combined_dataset)
