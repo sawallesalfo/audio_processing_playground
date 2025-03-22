af75f94f68a1d0893f08e33c464e5d929152a81b
Salif SAWADOGO <96554481+sawadogosalif@users.noreply.github.com>
2025-03-22 03:31:17 +0100

    👷  point to audio_paths

--- scripts/job_verbatim_dataset.py
+++ scripts/job_verbatim_dataset.py
@@ -44,6 +44,7 @@ def process_s3_audio_data(bucket_name: str, folder_to_process: str, output_path:
     datasets = []
     for file in files:
         try:
+            segmented_audio_folder = f"fasoai-segmented_audios/Sɩngre/{file.split(/)[-1].replace(".json","")}"
             download_folder_from_s3(s3_client, bucket_name, file)
             file_path = f"{bucket_name}/{file.as_posix()}"
             dataset = create_dataset_from_json(file_path, fs)
