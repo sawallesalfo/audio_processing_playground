b325850052fd028403592ab43dba5d5cfffb7524
sheicky <104073328+sheicky@users.noreply.github.com>
2025-03-24 23:25:45 +0100

    🚀  page1-2-jean

--- scripts/job_excel_contributor_dataset.py
+++ scripts/job_excel_contributor_dataset.py
@@ -103,7 +103,7 @@ if __name__ == "__main__":
     CHAPTER= "1-Zã"
     EXCEL_FILE= "contributor_files/1_Za.xlsx"
     PAGE_START = 1
-    PAGE_END = 5
+    PAGE_END = 2
     #######################################################################
     file_pattern = "page_{page}.mp3"
 
