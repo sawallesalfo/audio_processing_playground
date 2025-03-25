49f2a1eb0e563052a2b63942fc518b7ae6fceb1f
Salif SAWADOGO <96554481+sawadogosalif@users.noreply.github.com>
2025-03-25 01:25:47 +0100

    🚀  add 2_zan

--- scripts/job_excel_contributor_dataset.py
+++ scripts/job_excel_contributor_dataset.py
@@ -101,10 +101,10 @@ if __name__ == "__main__":
     DATA_FILE = "sawadogosalif/MooreFRCollections_BibleOnlyText"
     
     ################################### CHANGE ME ########################
-    CHAPTER= "1-Zã"
-    EXCEL_FILE= "contributor_files/1_Za.xlsx"
+    CHAPTER= "2-Zã"
+    EXCEL_FILE= "contributor_files/2_Za.xlsx"
     PAGE_START = 1
-    PAGE_END = 5
+    PAGE_END = 1
     #######################################################################
     file_pattern = "page_{page}.mp3"
 
