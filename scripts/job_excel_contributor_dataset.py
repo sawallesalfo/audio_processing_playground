2b525445e6b09e7adc86da5481d1316e4c427177
Salif SAWADOGO <96554481+sawadogosalif@users.noreply.github.com>
2025-03-29 23:31:01 +0100

    :beer: allez on fete

--- scripts/job_excel_contributor_dataset.py
+++ scripts/job_excel_contributor_dataset.py
@@ -100,10 +100,10 @@ if __name__ == "__main__":
     DATA_FILE = "sawadogosalif/MooreFRCollections_BibleOnlyText"
     
     ################################### CHANGE ME ########################
-    CHAPTER= "Zowɛll"
-    EXCEL_FILE= "contributor_files/Zowell_sebre.xlsx"
+    CHAPTER= "Ezekɩyɛll"
+    EXCEL_FILE= "contributor_files/Ezekɩyɛll.xlsx"
     PAGE_START = 1
-    PAGE_END = 3
+    PAGE_END = 30
 
     #######################################################################
     file_pattern = "page_{page}.mp3"
