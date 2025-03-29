a1d88be4e03233cade6259d41d2031be5321cec6
Salif SAWADOGO <96554481+sawadogosalif@users.noreply.github.com>
2025-03-29 02:18:50 +0100

    🚗 add abdiyaas

--- scripts/job_excel_contributor_dataset.py
+++ scripts/job_excel_contributor_dataset.py
@@ -99,10 +99,10 @@ if __name__ == "__main__":
     DATA_FILE = "sawadogosalif/MooreFRCollections_BibleOnlyText"
     
     ################################### CHANGE ME ########################
-    CHAPTER= "wiisgu"
-    EXCEL_FILE= "contributor_files/wiisgu.xlsx"
+    CHAPTER= "abdiyaas"
+    EXCEL_FILE= "contributor_files/abdiyaas.xlsx"
     PAGE_START = 1
-    PAGE_END = 5
+    PAGE_END = 1
     #######################################################################
     file_pattern = "page_{page}.mp3"
 
