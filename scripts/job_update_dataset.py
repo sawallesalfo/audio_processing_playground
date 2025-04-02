039086199261484875abfcd0d864bddeed68693c
Salif SAWADOGO <96554481+sawadogosalif@users.noreply.github.com>
2025-04-02 22:45:46 +0200

    🚀 Add ezayi

--- scripts/job_update_dataset.py
+++ scripts/job_update_dataset.py
@@ -72,8 +72,8 @@ if __name__ == "__main__":
     ########################## Change me ######################################
     VERBATIM = False 
     CURRENT_DATASET_PATH = "burkimbia/audio-dataset-aggregated"
-    COMMIT_MESSAGE = "🚀 Add 1pyerr"
-    INCOMING_DATASET_PATH = f"s3://{BUCKET_NAME}/hf_datasets/contribution_dataset_1-Pɩyɛɛr"
+    COMMIT_MESSAGE = "🚀 Add ezayi "
+    INCOMING_DATASET_PATH = f"s3://{BUCKET_NAME}/hf_datasets/contribution_dataset_ezayi"
     OUTPUT_DATASET_PATH = CURRENT_DATASET_PATH
     ############################################################################
 
