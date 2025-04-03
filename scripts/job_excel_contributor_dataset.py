7b97deccb69ed00a2e695733126eeafba8b2675c
Salif SAWADOGO <96554481+sawadogosalif@users.noreply.github.com>
2025-04-03 23:09:31 +0200

    ADD 1-Kibayã :rocket:

--- scripts/job_excel_contributor_dataset.py
+++ scripts/job_excel_contributor_dataset.py
@@ -98,10 +98,10 @@ if __name__ == "__main__":
     DATA_FILE = "sawadogosalif/MooreFRCollections_BibleOnlyText"
     
     ################################### CHANGE ME ########################
-    CHAPTER= "ezayi"
-    EXCEL_FILE= "contributor_files/esayi.xlsx"
+    CHAPTER= "1-Kibayã"
+    EXCEL_FILE= "contributor_files/1-kibaya.xlsx"
     PAGE_START = 1
-    PAGE_END = 66
+    PAGE_END = 29
 
     #######################################################################
     file_pattern = "page_{page}.mp3"
