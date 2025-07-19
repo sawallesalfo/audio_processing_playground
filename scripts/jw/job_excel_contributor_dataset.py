28ece4e0cc2fb71d7dbfc12d8e81e741ca5f6252
SALFO SAWALLE <96554481+sawallesalfo@users.noreply.github.com>
2025-07-20 00:51:22 +0200

    add sofoni

--- scripts/jw/job_excel_contributor_dataset.py
+++ scripts/jw/job_excel_contributor_dataset.py
@@ -103,10 +103,10 @@ if __name__ == "__main__":
     DATA_FILE = "sawadogosalif/MooreFRCollections_BibleOnlyText"
     
     ################################### CHANGE ME ########################
-    CHAPTER= "mark"
-    EXCEL_FILE= "contributor_files/MARK Sebre.xlsx"
+    CHAPTER= "sofoni"
+    EXCEL_FILE= "contributor_files/sofoni.xlsx"
     PAGE_START = 1
-    PAGE_END = 16
+    PAGE_END = 3
 
 
     #######################################################################
