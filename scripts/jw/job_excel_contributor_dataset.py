3a368263565288b671228024e044b0b1979fa4d5
SALFO SAWALLE <96554481+sawallesalfo@users.noreply.github.com>
2025-07-20 01:08:54 +0200

    ADD KORENT-RAMBA

--- scripts/jw/job_excel_contributor_dataset.py
+++ scripts/jw/job_excel_contributor_dataset.py
@@ -107,11 +107,10 @@ if __name__ == "__main__":
 
 
 
-
-     CHAPTER= "2-Kibayã"
-    EXCEL_FILE= "contributor_files/2-KIBAYA.xlsx"
+    CHAPTER= "2-Korẽnt-rãmbã"
+    EXCEL_FILE= "contributor_files/2-Korẽnt-rãmbã.xlsx"
     PAGE_START = 1
-    PAGE_END = 36
+    PAGE_END = 13
     #######################################################################
     file_pattern = "page_{page}.mp3"
     audio_files  = generate_audio_dict(PAGE_START, PAGE_END, "raw_data", CHAPTER, file_pattern)
