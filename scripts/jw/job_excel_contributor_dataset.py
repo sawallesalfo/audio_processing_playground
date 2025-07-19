c295e2d09a558d3021c52a2ea8d30eccc097c0a4
SALFO SAWALLE <96554481+sawallesalfo@users.noreply.github.com>
2025-07-20 01:46:26 +0200

    add aaze

--- scripts/jw/job_excel_contributor_dataset.py
+++ scripts/jw/job_excel_contributor_dataset.py
@@ -102,11 +102,10 @@ if __name__ == "__main__":
     DATA_FILE = "sawadogosalif/MooreFRCollections_BibleOnlyText"
     
     ################################### CHANGE ME ########################
-
-    CHAPTER= "zak"
-    EXCEL_FILE= "contributor_files/ZAK.xlsx"
+    CHAPTER= "aaze"
+    EXCEL_FILE= "contributor_files/AAZE.xlsx"
     PAGE_START = 1
-    PAGE_END = 5
+    PAGE_END = 2
     #######################################################################
     file_pattern = "page_{page}.mp3"
     audio_files  = generate_audio_dict(PAGE_START, PAGE_END, "raw_data", CHAPTER, file_pattern)
