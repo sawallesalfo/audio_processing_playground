97c728f5ffc11383cd43c86c48e297940d7da026
SALFO SAWALLE <96554481+sawallesalfo@users.noreply.github.com>
2025-07-20 02:00:47 +0200

    ADD 1-TIMOTE

--- scripts/jw/job_excel_contributor_dataset.py
+++ scripts/jw/job_excel_contributor_dataset.py
@@ -102,10 +102,10 @@ if __name__ == "__main__":
     DATA_FILE = "sawadogosalif/MooreFRCollections_BibleOnlyText"
     
     ################################### CHANGE ME ########################
-    CHAPTER= "yikri"
-    EXCEL_FILE= "contributor_files/yikiri.xlsx"
+    CHAPTER= "1-Tɩmote"
+    EXCEL_FILE= "contributor_files/1timotee.xlsx"
     PAGE_START = 1
-    PAGE_END = 3
+    PAGE_END = 6
     #######################################################################
     file_pattern = "page_{page}.mp3"
     audio_files  = generate_audio_dict(PAGE_START, PAGE_END, "raw_data", CHAPTER, file_pattern)
