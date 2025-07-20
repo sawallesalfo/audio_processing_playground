156a4303e8440e9f49aae39d0fa621c82a9ce0bb
SALFO SAWALLE <96554481+sawallesalfo@users.noreply.github.com>
2025-07-20 02:59:02 +0200

    AGGREGATION ɛsdras

--- scripts/jw/job_verbatim_to_long_audio.py
+++ scripts/jw/job_verbatim_to_long_audio.py
@@ -107,9 +107,9 @@ if __name__ == "__main__":
     
     BUCKET_NAME = "moore-collection"
     ##############################CHANGE ME ######################################
-    CHAPTER = "zakari"
-    FOLDER_TO_PROCESS = "output_jsons_zakari"
-    OUTPUT_PATH = f"s3://{BUCKET_NAME}/hf_datasets/audio-dataset-aggregated_zakari"
+    CHAPTER = "ɛsdras"
+    FOLDER_TO_PROCESS = "output_jsons_ɛsdras"
+    OUTPUT_PATH = f"s3://{BUCKET_NAME}/hf_datasets/audio-dataset-aggregated_ɛsdras"
     BATCH_SIZE= 7
     SILENCE_DURATION = 0.6
     #######################################################################################
